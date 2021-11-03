880c321c04eb9bf2a8ee8dd8d32ac8edba6308d2
devdutt-pedalsup <devdutt@pedalsup.com>
2021-11-03 17:22:28 +0530

    sended first chat

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000 , () => {
+var server = app.listen(3000 , () => {
     console.log('3000')
+})
+
+var io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made a socket connection')
+    console.log(socket.id)
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
 })
---
+++ public/chat.js
@@ -0,0 +1,18 @@
+const socket = io.connect('http://localhost:3000');
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const button = document.getElementById('send');
+const output = document.getElementById('output');
+
+button.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+    message.value = "";
+})
+
+socket.on('chat', (data) => {
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
